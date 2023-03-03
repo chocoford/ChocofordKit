720a0d64a665ced4eda9dfea59c7bf46f188f666
Chocoford <504001583@qq.com>
2023-03-03 16:50:02 +0800

    Create Image+Extension.swift

---
+++ Sources/ChocofordUI/Extensions/Image+Extension.swift
@@ -0,0 +1,32 @@
+//
+//  Image+Extension.swift
+//  TrickleAnyway
+//
+//  Created by Dove Zachary on 2023/2/20.
+//
+
+import SwiftUI
+
+extension Image {
+    init?(base64String: String) {
+        guard let data = Data(base64Encoded: base64String) else { return nil }
+        
+#if os(macOS)
+        guard let nsImage = NSImage(data: data) else { return nil }
+        self = Image(nsImage: nsImage)
+#elseif os(iOS)
+        guard let uiImage = UIImage(data: data) else { return nil }
+        self = Image(uiImage: uiImage)
+#endif
+    }
+    
+    init?(data: Data) {
+#if os(macOS)
+        guard let nsImage = NSImage(data: data) else { return nil }
+        self = Image(nsImage: nsImage)
+#elseif os(iOS)
+        guard let uiImage = UIImage(data: data) else { return nil }
+        self = Image(uiImage: uiImage)
+#endif
+    }
+}
