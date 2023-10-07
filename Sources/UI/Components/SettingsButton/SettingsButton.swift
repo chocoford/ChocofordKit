deaa844300694d9db83ad9c97d1a609058ce82c8
Chocoford <504001583@qq.com>
2023-10-07 13:05:21 +0800

    Update SettingsButton.swift

--- Sources/UI/Components/SettingsButton/SettingsButton.swift
+++ Sources/UI/Components/SettingsButton/SettingsButton.swift
@@ -10,18 +10,38 @@ import SwiftUI
 
 public struct SettingsButton<Label: View>: View {
     
+    var useDefaultLabel: Bool
+    var action: () -> Void
     var label: () -> Label
     
-    public init(@ViewBuilder label: @escaping () -> Label) {
+    /// Create a Settings Button
+    /// - Parameters:
+    ///   - useDefaultLabel: Tell `SettingsLink` to use default label. This parameter will be ignore before macOS Sonoma
+    ///   - followingAction: Perform action when button appear
+    ///   - label: The label of Settings button
+    public init(
+        useDefaultLabel: Bool = false,
+        followingAction: @escaping () -> Void = {},
+        @ViewBuilder label: @escaping () -> Label
+    ) {
+        self.useDefaultLabel = useDefaultLabel
+        self.action = followingAction
         self.label = label
     }
     
     public var body: some View {
-        if #available(macOS 14.0, *) {
-            SettingsLink(label: label)
-        } else if #available(macOS 10.15, *) {
-            Button(action: togglePreferenceView, label: label)
+        Group {
+            if #available(macOS 14.0, *) {
+                if useDefaultLabel {
+                    SettingsLink()
+                } else {
+                    SettingsLink(label: label)
+                }
+            } else if #available(macOS 10.15, *) {
+                Button(action: togglePreferenceView, label: label)
+            }
         }
+        .onAppear(perform: action)
     }
     
     public func togglePreferenceView() {
