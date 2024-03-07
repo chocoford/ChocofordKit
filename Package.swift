41533944d0818ebf6ce2b144076f7aef3e5a63cc
Chocoford <chocoford@icloud.com>
2024-03-07 19:43:42 +0800

    update SFSafeSymbol

--- Package.swift
+++ Package.swift
@@ -29,7 +29,7 @@ let package = Package(
         .package(url: "https://github.com/stevengharris/SplitView.git", from: "3.0.0"),
         .package(url: "https://github.com/fatbobman/SwiftUIOverlayContainer.git", from: "2.0.0"),
         .package(url: "https://github.com/lucaszischka/BottomSheet", from: "3.1.0"),
-        .package(url: "https://github.com/SFSafeSymbols/SFSafeSymbols.git", .upToNextMajor(from: "4.1.1")),
+        .package(url: "https://github.com/SFSafeSymbols/SFSafeSymbols.git", .upToNextMajor(from: "5.2.0")),
         .package(url: "https://github.com/lorenzofiamingo/swiftui-cached-async-image.git", .upToNextMajor(from: "2.1.1")),
         .package(url: "https://github.com/SDWebImage/SDWebImageSwiftUI.git", from: "2.2.2")
     ],
