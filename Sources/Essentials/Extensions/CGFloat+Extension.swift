be98efcf2048db584cbd2c76697634e48596fe1c
Chocoford <28218759+chocoford@users.noreply.github.com>
2025-05-23 17:14:00 +0800

    Update CGFloat+Extension.swift

--- Sources/Essentials/Extensions/CGFloat+Extension.swift
+++ Sources/Essentials/Extensions/CGFloat+Extension.swift
@@ -23,5 +23,41 @@ public extension CGFloat {
         return (keyWindow?.safeAreaInsets.top) ?? 0
         
     }
+    
+    static var safeArearBottom: CGFloat {
+        let keyWindow = UIApplication.shared.connectedScenes
+            .filter({$0.activationState == .foregroundActive})
+            .map({$0 as? UIWindowScene})
+            .compactMap({$0})
+            .first?.windows
+            .filter({$0.isKeyWindow}).first
+        
+        return (keyWindow?.safeAreaInsets.bottom) ?? 0
+        
+    }
+    
+    static var safeArearLeft: CGFloat {
+        let keyWindow = UIApplication.shared.connectedScenes
+            .filter({$0.activationState == .foregroundActive})
+            .map({$0 as? UIWindowScene})
+            .compactMap({$0})
+            .first?.windows
+            .filter({$0.isKeyWindow}).first
+        
+        return (keyWindow?.safeAreaInsets.left) ?? 0
+        
+    }
+    
+    static var safeArearRight: CGFloat {
+        let keyWindow = UIApplication.shared.connectedScenes
+            .filter({$0.activationState == .foregroundActive})
+            .map({$0 as? UIWindowScene})
+            .compactMap({$0})
+            .first?.windows
+            .filter({$0.isKeyWindow}).first
+        
+        return (keyWindow?.safeAreaInsets.right) ?? 0
+        
+    }
     #endif
 }
