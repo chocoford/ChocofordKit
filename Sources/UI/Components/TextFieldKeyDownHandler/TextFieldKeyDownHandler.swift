9d567ed409b4773e5839eeb5ad2951d201384464
Chocoford <chocoford@icloud.com>
2025-11-05 21:05:23 +0800

    Update TextFieldKeyDownHandler.swift

--- Sources/UI/Components/TextFieldKeyDownHandler/TextFieldKeyDownHandler.swift
+++ Sources/UI/Components/TextFieldKeyDownHandler/TextFieldKeyDownHandler.swift
@@ -190,6 +190,12 @@ struct TextFieldKeyDownEventMonitorModifier: ViewModifier {
                     }
                 }
             }
+            .onAppear {
+                addKeyDownListener()
+            }
+            .onDisappear {
+                removeKeyDownListener()
+            }
 //            .onChange(of: isFocused) { newValue in
 //                DispatchQueue.main.async {
 //                    if newValue, isEnabled {
